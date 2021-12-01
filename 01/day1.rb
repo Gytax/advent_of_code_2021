7ec411b8810f3d6d9dae0e6fd27563001839bb81
Kevin Ott <kevin.o.ott@gmail.com>
2021-12-01 16:46:05 +0100

    Day 1 - part 2

--- 01/day1.rb
+++ 01/day1.rb
@@ -1,8 +1,31 @@
-measurements        = File.read('input.txt').split.map(&:to_i)
-amount_of_increases = measurements.each.with_index.inject(0) do |sum, (measurement, i)|
-  next sum if i == 0 # Skip the first measaurement because it has no previous measurement
-  sum += 1 if measurement > measurements[i - 1]
-  sum
+def part1
+  amount_of_increases_in(get_measurements_from_file)
 end
 
-puts amount_of_increases
+def part2
+  original_measurements = get_measurements_from_file
+  measurements          = original_measurements.map.with_index do |measurement, i|
+    [measurement, original_measurements[i + 1], original_measurements[i + 2]]
+  end.reject do |arr|
+    arr.compact.length < 3
+  end.map(&:sum)
+
+  amount_of_increases_in(measurements)
+end
+
+def amount_of_increases_in(measurements)
+  increases = measurements.each.with_index.inject(0) do |sum, (measurement, i)|
+    next sum if i == 0 # Skip the first measaurement because it has no previous measurement
+    sum += 1 if measurement > measurements[i - 1]
+    sum
+  end
+
+  return increases
+end
+
+def get_measurements_from_file
+  measurements = File.read('input.txt').split.map(&:to_i)
+end
+
+puts "Amount of increases part1: #{part1}"
+puts "Amount of increases part2: #{part2}"
